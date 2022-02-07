4c6f29e8bf1a8007da31246839461e71a58b7304
Panagiotis <el15066@central.ntua.gr>
2022-02-07 17:22:13 +0200

    rename mutation -> Mutation (export)

--- ethdb/olddb/mutation.go
+++ ethdb/olddb/mutation.go
@@ -19,7 +19,7 @@ import (
 	// bench "github.com/ledgerwatch/erigon/bench"
 )
 
-type mutation struct {
+type Mutation struct {
 	puts       *btree.BTree
 	db         kv.RwTx
 	quit       <-chan struct{}
@@ -43,14 +43,14 @@ type MutationItem struct {
 // defer batch.Rollback()
 // ... some calculations on `batch`
 // batch.Commit()
-func NewBatch(tx kv.RwTx, quit <-chan struct{}) *mutation {
+func NewBatch(tx kv.RwTx, quit <-chan struct{}) *Mutation {
 	clean := func() {}
 	if quit == nil {
 		ch := make(chan struct{})
 		clean = func() { close(ch) }
 		quit = ch
 	}
-	return &mutation{
+	return &Mutation{
 		db:    tx,
 		puts:  btree.New(32),
 		quit:  quit,
@@ -67,14 +67,14 @@ func (mi *MutationItem) Less(than btree.Item) bool {
 	return bytes.Compare(mi.key, i.key) < 0
 }
 
-func (m *mutation) RwKV() kv.RwDB {
+func (m *Mutation) RwKV() kv.RwDB {
 	if casted, ok := m.db.(ethdb.HasRwKV); ok {
 		return casted.RwKV()
 	}
 	return nil
 }
 
-func (m *mutation) getMem(table string, key []byte) ([]byte, bool) {
+func (m *Mutation) getMem(table string, key []byte) ([]byte, bool) {
 	// bench.Tick(500) // expensive !
 	m.mu.RLock()
 	// bench.TiCk(501)
@@ -87,7 +87,7 @@ func (m *mutation) getMem(table string, key []byte) ([]byte, bool) {
 	return i.(*MutationItem).value, true
 }
 
-func (m *mutation) IncrementSequence(bucket string, amount uint64) (res uint64, err error) {
+func (m *Mutation) IncrementSequence(bucket string, amount uint64) (res uint64, err error) {
 	v, ok := m.getMem(kv.Sequence, []byte(bucket))
 	if !ok && m.db != nil {
 		v, err = m.db.GetOne(kv.Sequence, []byte(bucket))
@@ -109,7 +109,7 @@ func (m *mutation) IncrementSequence(bucket string, amount uint64) (res uint64, 
 
 	return currentV, nil
 }
-func (m *mutation) ReadSequence(bucket string) (res uint64, err error) {
+func (m *Mutation) ReadSequence(bucket string) (res uint64, err error) {
 	v, ok := m.getMem(kv.Sequence, []byte(bucket))
 	if !ok && m.db != nil {
 		v, err = m.db.GetOne(kv.Sequence, []byte(bucket))
@@ -126,7 +126,7 @@ func (m *mutation) ReadSequence(bucket string) (res uint64, err error) {
 }
 
 // Can only be called from the worker thread
-func (m *mutation) GetOne(table string, key []byte) ([]byte, error) {
+func (m *Mutation) GetOne(table string, key []byte) ([]byte, error) {
 	if value, ok := m.getMem(table, key); ok {
 		if value == nil {
 			return nil, nil
@@ -134,7 +134,7 @@ func (m *mutation) GetOne(table string, key []byte) ([]byte, error) {
 		return value, nil
 	}
 	if m.db != nil {
-		// TODO: simplify when tx can no longer be parent of mutation
+		// TODO: simplify when tx can no longer be parent of Mutation
 		value, err := m.db.GetOne(table, key)
 		if err != nil {
 			return nil, err
@@ -146,7 +146,7 @@ func (m *mutation) GetOne(table string, key []byte) ([]byte, error) {
 }
 
 // Can only be called from the worker thread
-func (m *mutation) Get(table string, key []byte) ([]byte, error) {
+func (m *Mutation) Get(table string, key []byte) ([]byte, error) {
 	value, err := m.GetOne(table, key)
 	if err != nil {
 		return nil, err
@@ -159,7 +159,7 @@ func (m *mutation) Get(table string, key []byte) ([]byte, error) {
 	return value, nil
 }
 
-func (m *mutation) Last(table string) ([]byte, []byte, error) {
+func (m *Mutation) Last(table string) ([]byte, []byte, error) {
 	c, err := m.db.Cursor(table)
 	if err != nil {
 		return nil, nil, err
@@ -168,7 +168,7 @@ func (m *mutation) Last(table string) ([]byte, []byte, error) {
 	return c.Last()
 }
 
-func (m *mutation) hasMem(table string, key []byte) bool {
+func (m *Mutation) hasMem(table string, key []byte) bool {
 	// bench.Tick(502)
 	m.mu.RLock()
 	// bench.TiCk(503)
@@ -177,7 +177,7 @@ func (m *mutation) hasMem(table string, key []byte) bool {
 	return m.puts.Has(&t)
 }
 
-func (m *mutation) Has(table string, key []byte) (bool, error) {
+func (m *Mutation) Has(table string, key []byte) (bool, error) {
 	if m.hasMem(table, key) {
 		return true, nil
 	}
@@ -187,7 +187,7 @@ func (m *mutation) Has(table string, key []byte) (bool, error) {
 	return false, nil
 }
 
-func (m *mutation) Put(table string, key []byte, value []byte) error {
+func (m *Mutation) Put(table string, key []byte, value []byte) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
@@ -201,36 +201,36 @@ func (m *mutation) Put(table string, key []byte, value []byte) error {
 	return nil
 }
 
-func (m *mutation) Append(table string, key []byte, value []byte) error {
+func (m *Mutation) Append(table string, key []byte, value []byte) error {
 	return m.Put(table, key, value)
 }
 
-func (m *mutation) AppendDup(table string, key []byte, value []byte) error {
+func (m *Mutation) AppendDup(table string, key []byte, value []byte) error {
 	return m.Put(table, key, value)
 }
 
-func (m *mutation) BatchSize() int {
+func (m *Mutation) BatchSize() int {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.size
 }
 
-func (m *mutation) ForEach(bucket string, fromPrefix []byte, walker func(k, v []byte) error) error {
+func (m *Mutation) ForEach(bucket string, fromPrefix []byte, walker func(k, v []byte) error) error {
 	m.panicOnEmptyDB()
 	return m.db.ForEach(bucket, fromPrefix, walker)
 }
 
-func (m *mutation) ForPrefix(bucket string, prefix []byte, walker func(k, v []byte) error) error {
+func (m *Mutation) ForPrefix(bucket string, prefix []byte, walker func(k, v []byte) error) error {
 	m.panicOnEmptyDB()
 	return m.db.ForPrefix(bucket, prefix, walker)
 }
 
-func (m *mutation) ForAmount(bucket string, prefix []byte, amount uint32, walker func(k, v []byte) error) error {
+func (m *Mutation) ForAmount(bucket string, prefix []byte, amount uint32, walker func(k, v []byte) error) error {
 	m.panicOnEmptyDB()
 	return m.db.ForAmount(bucket, prefix, amount, walker)
 }
 
-func (m *mutation) Delete(table string, k, v []byte) error {
+func (m *Mutation) Delete(table string, k, v []byte) error {
 	if v != nil {
 		return m.db.Delete(table, k, v) // TODO: mutation to support DupSort deletes
 	}
@@ -238,7 +238,7 @@ func (m *mutation) Delete(table string, k, v []byte) error {
 	return m.Put(table, k, nil)
 }
 
-func (m *mutation) doCommit(tx kv.RwTx) error {
+func (m *Mutation) doCommit(tx kv.RwTx) error {
 	var prevTable string
 	var c kv.RwCursor
 	var innerErr error
@@ -305,7 +305,7 @@ func (m *mutation) doCommit(tx kv.RwTx) error {
 	return innerErr
 }
 
-func (m *mutation) Commit() error {
+func (m *Mutation) Commit() error {
 
 	return nil // -readonly
 
@@ -324,7 +324,7 @@ func (m *mutation) Commit() error {
 	return nil
 }
 
-func (m *mutation) Rollback() {
+func (m *Mutation) Rollback() {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	m.puts.Clear(false /* addNodesToFreelist */)
@@ -332,20 +332,20 @@ func (m *mutation) Rollback() {
 	m.clean()
 }
 
-func (m *mutation) Close() {
+func (m *Mutation) Close() {
 	m.Rollback()
 }
 
-func (m *mutation) Begin(ctx context.Context, flags ethdb.TxFlags) (ethdb.DbWithPendingMutations, error) {
 	panic("mutation can't start transaction, because doesn't own it")
+func (m *Mutation) Begin(ctx context.Context, flags ethdb.TxFlags) (ethdb.DbWithPendingMutations, error) {
 }
 
-func (m *mutation) panicOnEmptyDB() {
+func (m *Mutation) panicOnEmptyDB() {
 	if m.db == nil {
 		panic("Not implemented")
 	}
 }
 
-func (m *mutation) SetRwKV(kv kv.RwDB) {
+func (m *Mutation) SetRwKV(kv kv.RwDB) {
 	m.db.(ethdb.HasRwKV).SetRwKV(kv)
 }
