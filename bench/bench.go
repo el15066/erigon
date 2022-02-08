5c94a6501d1a2aa525f12dc1c1301a2523182a5f
Panagiotis <el15066@central.ntua.gr>
2022-02-08 21:02:58 +0200

    bench: double+ read implementation of PrintAll()

--- bench/bench.go
+++ bench/bench.go
@@ -21,14 +21,13 @@ func Reset() {
 func Tick(index int) {
 	// all_ticks_prev[index] = all_ticks[index] // TODO: atomic copy
 	atomic.AddInt64(&all_counts[index], 1)
-	t := time.Now().UnixNano()
-	atomic.AddInt64(&all_ticks[ index], t)
+	atomic.AddInt64(&all_ticks[ index], time.Now().UnixNano())
 }
 
 func TiCk(index int) {
-	t := time.Now().UnixNano()
+	// t := time.Now().UnixNano()
 	// all_ticks_prev[index] = all_ticks[index] // TODO: atomic copy
-	atomic.AddInt64(&all_ticks[ index], t)
+	atomic.AddInt64(&all_ticks[ index], time.Now().UnixNano())
 	atomic.AddInt64(&all_counts[index], 1)
 }
 
@@ -60,13 +59,17 @@ func DiffAuto(indexA int, indexB int) int64 {
 	} else                                            { return Diff(indexA, false, indexB, false) }
 }
 
-func DiffStr(indexA int, isPrevA bool, indexB int, isPrevB bool) string {
-	ticksA, countsA := Get(indexA, isPrevA)
-	ticksB, countsB := Get(indexB, isPrevB)
+func DiffStrThese(ticksA, countsA, ticksB, countsB int64) string {
 	if countsA != countsB || countsA == 0 {
 		return "N/A"
 	}
 	return fmt.Sprintf("%14d %14d %14d", (ticksA - ticksB) / countsA, countsA, (ticksA - ticksB) / 1000)
+}
+
+func DiffStr(indexA int, isPrevA bool, indexB int, isPrevB bool) string {
+	ticksA, countsA := Get(indexA, isPrevA)
+	ticksB, countsB := Get(indexB, isPrevB)
+	return DiffStrThese(ticksA, countsA, ticksB, countsB)
 }
 
 func DiffStrAuto(indexA int, indexB int) string {
@@ -76,12 +79,40 @@ func DiffStrAuto(indexA int, indexB int) string {
 	return DiffStr(indexA, false, indexB, false)
 }
 
-func PrintAll() {
+func PrintAllNoCheck() {
 	fmt.Println("__TO-FROM ___AVERAGE_ns_ _______COUNT__ _____TOTAL_us_")
 	pairs := len(all_counts) - 1
 	for i := 0; i < pairs; i += 1 {
 		if all_counts[i] > 0 && all_counts[i+1] > 0 {
 			fmt.Println(fmt.Sprintf("%4d-%4d %s", i+1, i, DiffStrAuto(i+1, i)))
+		}
+	}
+}
+
+func PrintAll() {
+	fmt.Println("__TO-FROM ___AVERAGE_ns_ _______COUNT__ _____TOTAL_us_")
+	pairs := len(all_counts) - 1
+	for i := 0; i < pairs; i += 1 {
+		// we don't want to lock, because Tick() should be fast
+		// so we approximate by reading until no change
+		// it can still produce N/A but it's rare
+		var  c0,  c1,  t0,  t1 int64
+		var _c0, _c1, _t0, _t1 int64
+		for {
+			// atomic load here causes significant overhead, a measurable 0.5-1% in the whole execution benchmark (!!)
+			// c0 = atomic.LoadInt64(&all_counts[i])
+			// c1 = atomic.LoadInt64(&all_counts[i + 1])
+			// t0 = atomic.LoadInt64(&all_ticks[ i])
+			// t1 = atomic.LoadInt64(&all_ticks[ i + 1])
+			c0 = all_counts[i]
+			c1 = all_counts[i + 1]
+			t0 = all_ticks[ i]
+			t1 = all_ticks[ i + 1]
+			if c0 == _c0 && c1 == _c1 && t0 == _t0 && t1 == _t1 { break }
+			_c0, _c1, _t0, _t1 = c0, c1, t0, t1
+		}
+		if c0 > 0 && c1 > 0 {
+			fmt.Println(fmt.Sprintf("%4d-%4d %s", i+1, i, DiffStrThese(t1, c1, t0, c0)))
 		}
 	}
 }
