f768119bf75193505f11bbddec05f079e9fe5e22
Panagiotis <el15066@central.ntua.gr>
2022-02-14 19:51:12 +0200

    Update disasm.sh

--- logz/disasm.sh
+++ logz/disasm.sh
@@ -2,6 +2,7 @@
 
 disasm() {
     fname="$(basename "$1" | cut -d '.' -f 1)"
+    printf "\r$fname "
     evmasm -d -i "code/$fname.runbin.hex" -o "disasm/$fname.disasm"
 }
 
@@ -11,7 +12,14 @@ elif [ "$1" ]; then
     echo "Usage:" "$0"
     exit 1
 else
-    rm -r disasm/ || true
-    mkdir disasm  || true
-    find code/ -name '*.runbin.hex' -exec "$0" inner {} \;
+    mkdir disasm                           || true
+    printf 'Deleting *.disasm files in disasm/ : '
+    find  disasm/ -name '*.disasm' | wc -l || true
+    find  disasm/ -name '*.disasm' -delete || true
+    #find  code/   -name '*.runbin.hex' -exec "$0" inner {} \;
+    find  code/   -name '*.runbin.hex' | sort | while read -r fname
+    do
+        disasm "$fname"
+    done
+    echo $'\n''All files in code/ have been disassembled. The output files are in disasm/ .'
 fi
