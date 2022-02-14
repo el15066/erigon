ddec6e152f37e1e801d402cfe3b0f4671c3c81d6
Panagiotis <el15066@central.ntua.gr>
2022-02-14 04:01:31 +0200

    mutation_tree: refactoring

--- ethdb/olddb/mutation_tree/mutation_tree.go
+++ ethdb/olddb/mutation_tree/mutation_tree.go
@@ -12,39 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package btree implements in-memory B-Trees of arbitrary degree.
-//
-// btree implements an in-memory B-Tree for use as an ordered data structure.
-// It is not meant for persistent storage solutions.
-//
-// It has a flatter structure than an equivalent red-black or other binary tree,
-// which in some cases yields better memory usage and/or performance.
-// See some discussion on the matter here:
-//   http://google-opensource.blogspot.com/2013/01/c-containers-that-save-memory-and-time.html
-// Note, though, that this project is in no way related to the C++ B-Tree
-// implementation written about there.
-//
-// Within this tree, each node contains a slice of items and a (possibly nil)
-// slice of children.  For basic numeric values or raw structs, this can cause
-// efficiency differences when compared to equivalent C++ template code that
-// stores values in arrays within the node:
-//   * Due to the overhead of storing values as interfaces (each
-//     value needs to be stored as the value itself, then 2 words for the
-//     interface pointing to that value and its type), resulting in higher
-//     memory use.
-//   * Since interfaces can point to values anywhere in memory, values are
-//     most likely not stored in contiguous blocks, resulting in a higher
-//     number of cache misses.
-// These issues don't tend to matter, though, when working with strings or other
-// heap-allocated structures, since C++-equivalent structures also must store
-// pointers and also distribute their values across the heap.
-//
-// This implementation is designed to be a drop-in replacement to gollrb.LLRB
-// trees, (http://github.com/petar/gollrb), an excellent and probably the most
-// widely used ordered tree implementation in the Go ecosystem currently.
-// Its functions, therefore, exactly mirror those of
-// llrb.LLRB where possible.  Unlike gollrb, though, we currently don't
-// support storing multiple equivalent values.
+// Copied from https://github.com/google/btree
+// and modified for this specific use case.
+
 package mutation_tree
 
 import (
@@ -58,13 +28,7 @@ type MutationItem struct {
 }
 
 func (mi *MutationItem) Less(i *MutationItem) bool {
-	// i := than.(*MutationItem)
-	// c := mi.table - i.table
-	// if c != 0 {
-	// 	return c < 0
-	// }
 	return bytes.Compare(mi.Key, i.Key) < 0
-	// return string(mi.key) < string(i.key) // TODO
 }
 
 const (
@@ -78,10 +42,6 @@ var (
 	nilChildren = make(children, DEGREE)
 )
 
-// FreeList represents a free list of btree nodes. By default each
-// BTree has its own FreeList, but multiple BTrees can share the same
-// FreeList.
-// Two Btrees using the same freelist are safe for concurrent write access.
 type FreeList struct {
 	freelist []*node
 }
@@ -90,20 +50,13 @@ func newNode() (n *node) {
 	return new(node)
 }
 
-// New creates a new B-Tree with the given degree.
-//
-// New(2), for example, will create a 2-3-4 tree (each node contains 1-3 items
-// and 2-4 children).
 func New() *BTree {
 	return &BTree{}
 }
 
-// items stores items in a node.
 type items []*MutationItem
 
-// insertAt inserts a value into the given index, pushing all subsequent values
-// forward.
-func (s *items) insertAt(index int, item *MutationItem) {
+func (s *items) insertItemAt(index int, item *MutationItem) {
 	*s = append(*s, nil)
 	if index < len(*s) {
 		copy((*s)[index+1:], (*s)[index:])
@@ -111,20 +64,14 @@ func (s *items) insertAt(index int, item *MutationItem) {
 	(*s)[index] = item
 }
 
-func (s *items) truncate() {
+func (s *items) truncateItems() {
 	index   := MAX_ITEMS / 2
 	toClear := (*s)[index:]
 	*s       = (*s)[:index]
 	copy(toClear, nilItems)
-	// for len(toClear) > 0 {
-	// 	toClear = toClear[copy(toClear, nilItems):]
-	// }
 }
 
-// find returns the index where the given item should be inserted into this
-// list.  'found' is true if the item already exists in the list at the given
-// index.
-func (s items) find(item *MutationItem) (index int, found bool) {
+func (s items) find(item *MutationItem) (int, bool) {
 	i := sort.Search(len(s), func(i int) bool {
 		return item.Less(s[i])
 	})
@@ -134,12 +81,9 @@ func (s items) find(item *MutationItem) (index int, found bool) {
 	return i, false
 }
 
-// children stores child nodes in a node.
 type children []*node
 
-// insertAt inserts a value into the given index, pushing all subsequent values
-// forward.
-func (s *children) insertAt(index int, n *node) {
+func (s *children) insertChildAt(index int, n *node) {
 	*s = append(*s, nil)
 	if index < len(*s) {
 		copy((*s)[index+1:], (*s)[index:])
@@ -147,21 +91,14 @@ func (s *children) insertAt(index int, n *node) {
 	(*s)[index] = n
 }
 
-func (s *children) truncate() {
+func (s *children) truncateChildren() {
 	index   := MAX_ITEMS / 2 + 1
 	toClear := (*s)[index:]
 	*s       = (*s)[:index]
 	copy(toClear, nilChildren)
-	// var toClear children
-	// *s, toClear = (*s)[:index], (*s)[index:]
-	// for len(toClear) > 0 {
-	// 	toClear = toClear[copy(toClear, nilChildren):]
-	// }
 }
 
-// node is an internal node in a tree.
-//
-// It must at all times maintain the invariant that either
+// `node` must at all times maintain the invariant that either
 //   * len(children) == 0, len(items) unconstrained
 //   * len(children) == len(items) + 1
 type node struct {
@@ -169,38 +106,30 @@ type node struct {
 	children children
 }
 
-// split splits the given node at the given index.  The current node shrinks,
-// and this function returns the item that existed at that index and a new node
-// containing all items/children after it.
 func (n *node) split() (*MutationItem, *node) {
 	i    := MAX_ITEMS / 2
 	item := n.items[i]
 	next := newNode()
 	next.items = append(next.items, n.items[i+1:]...)
-	n.items.truncate()
+	n.items.truncateItems()
 	if len(n.children) > 0 {
 		next.children = append(next.children, n.children[i+1:]...)
-		n.children.truncate()
+		n.children.truncateChildren()
 	}
 	return item, next
 }
 
-// maybeSplitChild checks if a child should be split, and if so splits it.
-// Returns whether or not a split occurred.
 func (n *node) maybeSplitChild(i int) bool {
 	if len(n.children[i].items) < MAX_ITEMS {
 		return false
 	}
 	first := n.children[i]
 	item, second := first.split()
-	n.items.insertAt(i, item)
-	n.children.insertAt(i+1, second)
+	n.items.insertItemAt(i, item)
+	n.children.insertChildAt(i+1, second)
 	return true
 }
 
-// insert inserts an item into the subtree rooted at this node, making sure
-// no nodes in the subtree exceed MAX_ITEMS items.  Should an equivalent item be
-// be found/replaced by insert, it will be returned.
 func (n *node) insert(item *MutationItem) *MutationItem {
 	i, found := n.items.find(item)
 	if found {
@@ -209,7 +138,7 @@ func (n *node) insert(item *MutationItem) *MutationItem {
 		return out
 	}
 	if len(n.children) == 0 {
-		n.items.insertAt(i, item)
+		n.items.insertItemAt(i, item)
 		return nil
 	}
 	if n.maybeSplitChild(i) {
@@ -228,7 +157,6 @@ func (n *node) insert(item *MutationItem) *MutationItem {
 	return n.children[i].insert(item)
 }
 
-// get finds the given key in the subtree and returns it.
 func (n *node) get(key *MutationItem) *MutationItem {
 	i, found := n.items.find(key)
 	if found {
@@ -239,26 +167,11 @@ func (n *node) get(key *MutationItem) *MutationItem {
 	return nil
 }
 
-// BTree is an implementation of a B-Tree.
-//
-// BTree stores *MutationItem instances in an ordered structure, allowing easy insertion,
-// removal, and iteration.
-//
-// Write operations are not safe for concurrent mutation by multiple
-// goroutines, but Read operations are.
 type BTree struct {
 	root *node
 }
 
-// ReplaceOrInsert adds the given item to the tree.  If an item in the tree
-// already equals the given one, it is removed from the tree and returned.
-// Otherwise, nil is returned.
-//
-// nil cannot be added to the tree (will panic).
 func (t *BTree) ReplaceOrInsert(item *MutationItem) *MutationItem {
-	if item == nil {
-		panic("nil item being added to BTree")
-	}
 	if t.root == nil {
 		t.root       = newNode()
 		t.root.items = append(t.root.items, item)
@@ -275,8 +188,6 @@ func (t *BTree) ReplaceOrInsert(item *MutationItem) *MutationItem {
 	return t.root.insert(item)
 }
 
-// Get looks for the key item in the tree, returning it.  It returns nil if
-// unable to find that item.
 func (t *BTree) Get(key *MutationItem) *MutationItem {
 	if t.root == nil {
 		return nil
@@ -284,7 +195,6 @@ func (t *BTree) Get(key *MutationItem) *MutationItem {
 	return t.root.get(key)
 }
 
-// Has returns true if the given key is in the tree.
 func (t *BTree) Has(key *MutationItem) bool {
 	return t.Get(key) != nil
 }
