6f4fb2281401ab81bca14bacca62910aa2f57b4a
Panagiotis <el15066@central.ntua.gr>
2022-02-14 05:07:09 +0200

    mutation_tree: fixed size arrays in node

--- ethdb/olddb/mutation_tree/mutation_tree.go
+++ ethdb/olddb/mutation_tree/mutation_tree.go
@@ -37,10 +37,8 @@ const (
 	MAX_ITEMS     = DEGREE * 2 - 1
 )
 
-var (
-	nilItems    = make([]*MutationItem, DEGREE)
-	nilChildren = make([]*node,         DEGREE)
-)
+// var nilItems    = [DEGREE]*MutationItem{}
+// var nilChildren = [DEGREE]*node{}
 
 type FreeList struct {
 	freelist []*node
@@ -54,37 +52,32 @@ func New() *BTree {
 //   * len(children) == 0, len(items) unconstrained
 //   * len(children) == len(items) + 1
 type node struct {
-	// n_items    int
-	// n_children int
-	items    []*MutationItem
-	children []*node
+	items_len    int
+	children_len int
+	items        [MAX_ITEMS]*MutationItem
+	children     [MAX_ITEMS+1]*node
 }
 
 func newNode() (n *node) {
-	return &node{
-		items:    make([]*MutationItem, 0, MAX_ITEMS),
-		children: make([]*node,         0, MAX_ITEMS + 1),
-	}
+	return new(node)
 }
 
 func (n *node) insertItemAt(i int, item *MutationItem) {
-	l := len(n.items)
-	n.items = n.items[:l+1]
-	if i <= l {
+	if i <= n.items_len {
 		copy(n.items[i+1:], n.items[i:])
 	}
-	n.items[i] = item
+	n.items_len += 1
+	n.items[i]   = item
 }
 
 func (n *node) truncateItems() {
-	i       := MAX_ITEMS / 2
-	toClear := n.items[i:]
-	n.items  = n.items[:i]
-	copy(toClear, nilItems)
+	const i     = MAX_ITEMS / 2
+	n.items_len = i
+	for j := i; j < len(n.items); j += 1 { n.items[j] = nil }
 }
 
 func (n *node) findItem(item *MutationItem) (int, bool) {
-	i := sort.Search(len(n.items), func(j int) bool {
+	i := sort.Search(n.items_len, func(j int) bool {
 		return item.Less(n.items[j])
 	})
 	if i > 0 && !n.items[i-1].Less(item) {
@@ -94,36 +87,38 @@ func (n *node) findItem(item *MutationItem) (int, bool) {
 }
 
 func (n *node) insertChildAt(i int, child *node) {
-	l := len(n.children)
-	n.children = n.children[:l+1]
-	if i <= l {
+	if i <= n.children_len {
 		copy(n.children[i+1:], n.children[i:])
 	}
-	n.children[i] = child
+	n.children_len += 1
+	n.children[i]   = child
 }
 
 func (n *node) truncateChildren() {
-	i          := MAX_ITEMS / 2 + 1
-	toClear    := n.children[i:]
-	n.children  = n.children[:i]
-	copy(toClear, nilChildren)
+	const i        = MAX_ITEMS / 2 + 1
+	n.children_len = i
+	for j := i; j < len(n.children); j += 1 { n.children[j] = nil }
 }
 
 func (n *node) split() (*MutationItem, *node) {
-	i    := MAX_ITEMS / 2
+	const i = MAX_ITEMS / 2
 	item := n.items[i]
 	next := newNode()
-	next.items = append(next.items, n.items[i+1:]...)
+	//
+	next.items_len = MAX_ITEMS-i-1
+	copy(next.items[:MAX_ITEMS-i-1], n.items[i+1:])
 	n.truncateItems()
-	if len(n.children) > 0 {
-		next.children = append(next.children, n.children[i+1:]...)
+	//
+	if n.children_len > 0 {
+		next.children_len = MAX_ITEMS-i
+		copy(next.children[:MAX_ITEMS-i], n.children[i+1:])
 		n.truncateChildren()
 	}
 	return item, next
 }
 
 func (n *node) maybeSplitChild(i int) bool {
-	if len(n.children[i].items) < MAX_ITEMS {
+	if n.children[i].items_len < MAX_ITEMS {
 		return false
 	}
 	first := n.children[i]
@@ -140,7 +135,7 @@ func (n *node) insert(item *MutationItem) *MutationItem {
 		n.items[i] = item
 		return out
 	}
-	if len(n.children) == 0 {
+	if n.children_len == 0 {
 		n.insertItemAt(i, item)
 		return nil
 	}
@@ -164,7 +159,7 @@ func (n *node) get(key *MutationItem) *MutationItem {
 	i, found := n.findItem(key)
 	if found {
 		return n.items[i]
-	} else if len(n.children) > 0 {
+	} else if n.children_len > 0 {
 		return n.children[i].get(key)
 	}
 	return nil
@@ -176,16 +171,20 @@ type BTree struct {
 
 func (t *BTree) ReplaceOrInsert(item *MutationItem) *MutationItem {
 	if t.root == nil {
-		t.root       = newNode()
-		t.root.items = append(t.root.items, item)
+		t.root           = newNode()
+		t.root.items_len = 1
+		t.root.items[0]  = item
 		return nil
 	} else {
-		if len(t.root.items) >= MAX_ITEMS {
-			item2, second  := t.root.split()
-			oldroot        := t.root
-			t.root          = newNode()
-			t.root.items    = append(t.root.items, item2)
-			t.root.children = append(t.root.children, oldroot, second)
+		if t.root.items_len >= MAX_ITEMS {
+			item2, second       := t.root.split()
+			oldroot             := t.root
+			t.root               = newNode()
+			t.root.items_len     = 1
+			t.root.items[0]      = item2
+			t.root.children_len  = 2
+			t.root.children[0]   = oldroot
+			t.root.children[1]   = second
 		}
 	}
 	return t.root.insert(item)
