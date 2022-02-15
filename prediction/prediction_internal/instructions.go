1b0fd986b35e8e4e7daee13e31154b81960e9f56
Panagiotis <el15066@central.ntua.gr>
2022-02-16 00:49:50 +0200

    prediction: use callvalue in opSelfBalance

--- prediction/prediction_internal/instructions.go
+++ prediction/prediction_internal/instructions.go
@@ -205,7 +205,8 @@ func opGasLimit(state *State) {
 }
 func opSelfBalance(state *State) {
 	d := zerOpArgVs(state)
-	d.Set(&UNKNOWN_U256) // set to unknown, to continue predicting even if it wouldn't
+	d.Set(&state.callvalue)
+	// d.Set(&UNKNOWN_U256) // set to unknown, to continue predicting even if it wouldn't
 	return
 }
 
