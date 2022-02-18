5b4f2c8d38b422cc1f0818b32c891b4339d80fc2
Panagiotis <el15066@central.ntua.gr>
2022-02-18 11:53:28 +0200

    mutation: add bench Tick 220-221 for Put()

--- ethdb/olddb/mutation.go
+++ ethdb/olddb/mutation.go
@@ -16,7 +16,7 @@ import (
 	"github.com/ledgerwatch/erigon/ethdb"
 	// "github.com/ledgerwatch/log/v3"
 
-	// "github.com/ledgerwatch/erigon/bench"
+	"github.com/ledgerwatch/erigon/bench"
 )
 
 var BucketsMap  = map[string]int{}
@@ -202,8 +202,9 @@ func (m *Mutation) Has(table string, key []byte) (bool, error) {
 }
 
 func (m *Mutation) Put(table string, key []byte, value []byte) error {
+	bench.Tick(220) // expensive !
 	m.mu.Lock()
-	defer m.mu.Unlock()
+	bench.TiCk(221)
 
 	newMi := mtree.MutationItem{ key, value }
 	i := m.trees[tableNameToID(table)].ReplaceOrInsert(newMi)
@@ -211,6 +212,8 @@ func (m *Mutation) Put(table string, key []byte, value []byte) error {
 	if i.Key != nil {
 		m.size -= (int(unsafe.Sizeof(i)) + len(i.Key) + len(i.Value))
 	}
+
+	m.mu.Unlock()
 	return nil
 }
 
