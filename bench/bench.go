778d09ff4456ee19c240c2b69ef26f65c5dd43a3
Panagiotis <el15066@central.ntua.gr>
2022-02-18 18:09:58 +0200

    bench: use TSC

--- bench/bench.go
+++ bench/bench.go
@@ -7,14 +7,17 @@ import (
 
 	// mclock "github.com/ledgerwatch/erigon/common/mclock"
 
-	_ "unsafe" // for go:linkname
+	// _ "unsafe" // for go:linkname
 
-	// tsc "github.com/dterei/gotsc"
+	tsc "github.com/el15066/gotsc"
 )
 
-//go:noescape
-//go:linkname Nanotime runtime.nanotime
-func Nanotime() int64 // on linux: __vdso_clock_gettime
+// The TSC frequency, usually the advertised base frequency
+const TSC_FREQ_100MHZ = 38 // 3.8 GHz
+
+// //go:noescape
+// //go:linkname Nanotime runtime.nanotime
+// func Nanotime() int64 // on linux: __vdso_clock_gettime
 
 var all_ticks      [300]int64
 var all_ticks_prev [300]int64
@@ -33,9 +36,10 @@ func Tick(index int) {
 	atomic.AddInt64(&all_counts[index], 1)
 	// t := time.Now().UnixNano()
 	// t := int64(mclock.Now())
-	t := Nanotime()
-	// t := int64(tsc.BenchStart())
-	atomic.AddInt64(&all_ticks[ index], t) // atomic.* have mem barriers, so order matters
+	// t := Nanotime()
+	// _  = all_ticks[ index] // cache it // unfortunately this kills inlining (and is optimized out)
+	t := int64(tsc.BenchStart())
+	atomic.AddInt64(&all_ticks[ index], t)
 }
 
 // func TiCk(index int) { Tick(index) }
@@ -43,8 +47,8 @@ func Tick(index int) {
 func TiCk(index int) {
 	// t := time.Now().UnixNano()
 	// t := int64(mclock.Now())
-	t := Nanotime()
-	// t := int64(tsc.BenchEnd())
+	// t := Nanotime()
+	t := int64(tsc.BenchEnd())
 	// all_ticks_prev[index] = all_ticks[index] // TODO: atomic copy
 	atomic.AddInt64(&all_ticks[ index], t)
 	atomic.AddInt64(&all_counts[index], 1)
@@ -82,7 +86,16 @@ func DiffStrThese(ticksA, countsA, ticksB, countsB int64) string {
 	if countsA != countsB || countsA == 0 {
 		return "N/A"
 	}
-	return fmt.Sprintf("%14d %14d %14d", (ticksA - ticksB) / countsA, countsA, (ticksA - ticksB) / 1000)
+	//
+	if TSC_FREQ_100MHZ == 0 {
+		return fmt.Sprintf("%14d %14d %14d", (ticksA - ticksB) / countsA, countsA, (ticksA - ticksB) / 1000)
+	} else {
+		return fmt.Sprintf("%14d %14d %14d",
+			((ticksA - ticksB) * 10) / (TSC_FREQ_100MHZ * countsA),
+			countsA,
+			 (ticksA - ticksB)       / (TSC_FREQ_100MHZ * 100),
+		)
+	}
 }
 
 func DiffStr(indexA int, isPrevA bool, indexB int, isPrevB bool) string {
@@ -98,15 +111,15 @@ func DiffStrAuto(indexA int, indexB int) string {
 	return DiffStr(indexA, false, indexB, false)
 }
 
-func PrintAllNoCheck() {
-	fmt.Println("__TO-FROM ___AVERAGE_ns_ _______COUNT__ _____TOTAL_us_")
-	pairs := len(all_counts) - 1
-	for i := 0; i < pairs; i += 1 {
-		if all_counts[i] > 0 && all_counts[i+1] > 0 {
-			fmt.Println(fmt.Sprintf("%4d-%4d %s", i+1, i, DiffStrAuto(i+1, i)))
-		}
-	}
-}
+// func PrintAllNoCheck() {
+// 	fmt.Println("__TO-FROM ___AVERAGE_ns_ _______COUNT__ _____TOTAL_us_")
+// 	pairs := len(all_counts) - 1
+// 	for i := 0; i < pairs; i += 1 {
+// 		if all_counts[i] > 0 && all_counts[i+1] > 0 {
+// 			fmt.Println(fmt.Sprintf("%4d-%4d %s", i+1, i, DiffStrAuto(i+1, i)))
+// 		}
+// 	}
+// }
 
 func PrintAll() {
 	fmt.Println("__TO-FROM ___AVERAGE_ns_ _______COUNT__ _____TOTAL_us_")
